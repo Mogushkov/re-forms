8547fc3b56644b1d1ef48ef6c2c81c95569b12fb
Gapur Mogushkov <gapur@mail.ru>
2021-12-13 22:16:50 +0300

    changes

--- src/components/converter/Converter.jsx
+++ src/components/converter/Converter.jsx
@@ -1,31 +1,29 @@
 import { useState } from 'react';
-import valid from '../validate/validate';
 
-function Converter() {
-const [color, setColor] = useState({
-    rgb: '',   
-})
+export default function Converter() {
+  const [color, setColor] = useState('');
 
-    const onCheck = (e) => {
-        e.preventDefault();
+  const handleChange = ({ target }) => {
+    if (target.value.length < 7) {
+      return;
     }
 
-    const onValide = (e) => {
-        if (e.target.value.length >= 7) {
-            setColor(prev => ({rgb: valid(e.target.value) || 'Ошибка!'}))    
-        } else {
-            setColor(prev => ({rgb: ''}))
-        }
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(target.value);
+    if (result === null) {
+      setColor('Ошибка!');
+      return;
     }
 
-    return (
-        <div className="container" style={{backgroundColor: color.rgb}}>
-            <form className="form" onSubmit={onCheck}>
-                <input className="input field" type="text" name="hex" onChange={onValide}></input>
-                <output className="output field" name="rgb">{color.rgb}</output>
-            </form>    
-        </div>
-    )
+    const rgb = `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}`;
+    setColor(`rgb(${rgb})`);
+  }
+
+  return (
+    <div className="Converter" style={{ backgroundColor: color }}>
+      <div className="Converter__container">
+        <input className="Converter__input" onChange={handleChange} />
+        <div className="Converter__result">{color}</div>
+      </div>
+    </div>
+  );
 }
-
-export default Converter
